3ade86863205d9b165f319b2d1575cd44c00b6cd
bradius <mike@bradius.ru>
2013-03-27 08:19:38 +0400

    showCategories, showSections

--- published/include/maincolumn/tmpl/pages/index.php
+++ published/include/maincolumn/tmpl/pages/index.php
@@ -18,6 +18,30 @@
 			}
 			return $result;
 		}
+        function showCategories($id){
+            $html = "";
+            $sql = mysql_query("select category from pages where id=".$id);
+            $row = mysql_fetch_array($sql);
+            $option = $row["category"];
+            $sql = mysql_query("select * from categories");
+            while($row = mysql_fetch_array($sql)){
+                if($row["id"] == $option) $html .= "<option selected value=".$row["id"].">".$row["name"]."</option>";
+                else $html .= "<option value='".$row["id"]."'>".$row["name"]."</option>";
+            }
+            return $html;
+        }
+        function showSections($id){
+            $html = "";
+            $sql = mysql_query("select section from pages where id =".$id);
+            $row = mysql_fetch_array($sql);
+            $option = $row["section"];
+            $sql = mysql_query("select * from sections");
+            while($row = mysql_fetch_array($sql)){
+                if($row["id"] == $option) $html .= "<option selected value=".$row["id"].">".$row["name"]."</option>";
+                else $html .= "<option value='".$row["id"]."'>".$row["name"]."</option>";
+            }
+            return $html;
+        }
 	}
 	
 
